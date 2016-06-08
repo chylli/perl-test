cf241f898d8c092ad0ac8a65adaa1c2a9a399e04
Chylli <chylli@binary.com>
2016-06-08 16:08:51 +0800

    setup

---
+++ memory_move/test.pl
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+
+use Benchmark qw(timethis timeit :hireswallclock );
+use File::Slurp;
+use Data::Dumper;
+use IO::Null;
+
+my $out;
+my $null = IO::Null->new;
+my $stdout = select($null);
+
+my @a = (0) x 1024;
+sub test {
+  my @b = ([@a]);
+  for my $i (1..10){
+    $b[$i] = [@a];
+    $b[$i-1] = [];
+  }
+}
+
+$out = timethis(-20, sub{test()});
+
+
+select($stdout);
+print Dumper $out;
---
+++ setup_cpan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BINARY=~/work/git/regentmarkets
+PERL=$BINARY/perl
+CPAN=$BINARY/localcpan
+for v in v5.14.4-v3 v5.22.2-v2 v5.22.2-v3 v.24.0-v2 v5.24.0-v3
+do
+    (cd $PERL && git checkout chylli/$v)
+    perl -v
+    (cd $CPAN && git checkout chylli/$v && git checkout chylli/v5.14.4-v2 cpanfile && carton install)
+done
